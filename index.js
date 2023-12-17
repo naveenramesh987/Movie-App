7b66dad0fc7692a6a9a70a914ac7e9ef8464e658
Naveen Ramesh <44354099+naveenramesh987@users.noreply.github.com>
2023-12-16 18:28:48 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const password = process.env.MONGO_PASSWORD;
 
 import app from "./server.js"
 import mongodb from "mongodb"
-import ReviewsDAO from "./dao/reviewsDAO.js"
+import ReviewsDAO from "./reviewsDAO.js"
 
 const MongoClient = mongodb.MongoClient
 const mongo_username = process.env['MONGO_USERNAME']
